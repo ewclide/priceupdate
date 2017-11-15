5ec4b1981e791fcb27be99fc4a6aea454b4d834f
ewclide <ewclide@yandex.ru>
2017-11-15 13:35:06 +0300

    update to es6

---
+++ index.php
@@ -0,0 +1,118 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script src="jquery.js"></script>
+	<script src="priceupdate.js"></script>
+</head>
+<body>
+	<p 
+	id="old_price"
+	data-price-value="7000"
+	>7000 р.</p>
+
+	<p 
+	class="priceupdate"
+	data-price-old="#old_price"
+	data-price-points="price"
+	data-price-value="5000"
+	data-price-currency=" р."
+	>5000 р.</p>
+
+	<p 
+	class="priceupdate"
+	data-price-points="price2"
+	data-price-value="5000"
+	data-price-currency=" р."
+	>5000 р.</p>
+
+	<div>
+		<div>
+			
+			<input  
+			data-price-target="price"
+			data-price-operator="+"
+			data-price-value="2000"
+			type="checkbox"
+			name="checkbox"
+			>
+			<label>+2000</label>
+
+			
+			<input  
+			data-price-target="price"
+			data-price-operator="-"
+			data-price-value="3000"
+			type="checkbox"
+			name="checkbox"
+			>
+			<label>-3000</label>
+
+			
+			<input  
+			data-price-target="price"
+			data-price-operator="+%"
+			data-price-value="10"
+			type="checkbox"
+			name="checkbox"
+			>
+			<label>+10%</label>
+
+			
+			<input  
+			data-price-target="price2"
+			data-price-operator="-"
+			data-price-value="3000"
+			type="checkbox"
+			name="checkbox"
+			>
+			<label>-3000</label>
+		</div>
+		<div>
+			
+			<input  
+			data-price-target="price"
+			data-price-operator="+"
+			data-price-value="3000"
+			type="radio"
+			name="radio"
+			>
+			<label>+3000</label>
+
+			
+			<input  
+			data-price-target="price"
+			data-price-operator="+"
+			data-price-value="1000"
+			type="radio"
+			name="radio"
+			>
+			<label>+1000</label>
+
+			
+			<input  
+			data-price-target="price"
+			data-price-operator="-"
+			data-price-value="2000"
+			type="radio"
+			name="radio"
+			>
+			<label>-2000</label>
+			
+		</div>
+		<div>
+			<select
+			data-price-target="price"
+			name="select"
+			>
+				<option>не выбрано</option>
+				<option data-price-operator="+" data-price-value="1500">+ 1500</option>
+				<option data-price-operator="-" data-price-value="1000">- 1000</option>
+				<option data-price-operator="*" data-price-value="2">* 2</option>
+			</select>
+		</div>
+		
+	</div>
+</body>
+</html>
--- priceupdate.js
+++ priceupdate.js
@@ -1,203 +1,248 @@
 (function(){
-    function Option(options)
-    {
-        this.id = Math.random();
-        this.parent = null;
-        this.elem = null;
-        this.operator = null;
-        this.price = null;
-        this.calctype = null;
-        this.init(options);
-    }
-    Option.prototype.init = function(options)
-    {
-        var self = this;
-        this.parent = options.parent;
-        this.elem = options.elem;
-        if (this.elem)
-        {
-            this.name = this.elem.attr('name');
-            this.type = this.elem.attr('type') || this.elem[0].nodeName.toLowerCase();
-            this.operator = this.elem.attr('data-price-operator') || "+";
-            this.calctype = this.elem.attr('data-price-calctype') || "update";
-            this.price = (function(){
-                    var price = self.elem.attr('data-price-value');
-                    if (price) return parseFloat(price);
-                    else return 0;
-                })();
-            checkChoosen(this.type);
-            this.elem.on("change", function(){
-                if (self.type == "select") getSelectData();
-                self.parent.recalc(self);
-            })
-        }
-        else console.error("calcPrice target option reference to undefined element");
 
-        function checkChoosen(type)
+    class Option
+    {
+        constructor(options)
+        {
+            this.id = Math.random();
+            this.parent;
+            this.$element;
+            this.operator;
+            this.price;
+            this.calctype;
+            this._init(options);
+        }
+
+        _init(options)
+        {
+            var self = this;
+
+            this.parent = options.parent;
+            this.$element = options.$element;
+
+            if (this.$element)
+            {
+                this.name = this.$element.attr('name');
+                this.type = this.$element.attr('type') || this.$element[0].nodeName.toLowerCase();
+                this.operator = this.$element.attr('data-price-operator') || "+";
+                this.calctype = this.$element.attr('data-price-calctype') || "update";
+                this.price = (function(){
+                        var price = self.$element.attr('data-price-value');
+                        if (price) return parseFloat(price);
+                        else return 0;
+                    })();
+                    
+                this._checkChoosen(this.type);
+
+                this.$element.on("change", function(){
+                    if (self.type == "select") self._getSelectData();
+                    self.parent.recalc(self);
+                })
+            }
+            else
+            {
+                console.error("calcPrice target option reference to undefined element");
+            }
+        }
+
+        _checkChoosen(type)
         {
             if (type == "select")
             {
-                getSelectData();
-                if (self.price) self.parent.recalc(self);
+                this._getSelectData();
+
+                if (this.price) this.parent.recalc(this);
             }
-            else if (type == "radio" || type == "checkbox")
+            else if (this == "radio" || type == "checkbox")
             {
-                if (self.elem.attr("checked")) self.parent.recalc(self);
+                if (this.$element.attr("checked")) this.parent.recalc(this);
             }
         }
 
-        function getSelectData()
+        _getSelectData()
         {
-            var option = self.elem.find('option:selected');
-            self.operator = option.attr('data-price-operator');
-            self.price = options.price || (function(){
-                    var price = option.attr('data-price-value');
-                    if (price) return parseFloat(price);
-                    else return 0;
-                })();
-        }
-    }
+            var option = this.$element.find('option:selected');
 
-    function Price(options)
-    {
-        this.elem = null;
-        this.price = null;
-        this.currency = null;
-        this.updatePrice = null;
-        this.points = null;
-        this.old = null;
-        this.oldPrice = null;
-        this.options = {};
-        this.factor = null;
-        this.init(options);
-    }
-    Price.prototype.init = function(options)
-    {
-        var self = this;
-        this.elem = options.elem;
-        if (this.elem)
-        {
-            this.price = parseFloat(this.elem.attr("data-price-value")) || 0;
-            this.currency = this.elem.attr('data-price-currency') || "";
-            this.old = (function(){
-                    var old = self.elem.attr('data-price-old');
-                        old ? old = $(old) : old = false;
-                    if (old) return old;
-                    else return false;
-                })();
-            this.oldPrice = self.old ? parseFloat(self.old.attr("data-price-value")) || 0 : 0;
-            this.factor = (function(){
-                    if (self.old) return self.oldPrice / self.price;
-                    else return 0;
-                })();
-            this.points = (function(){
-                var result = [];
-                var points = self.elem.attr('data-price-points') || false;
-                    points ? points = $("[data-price-target='" + points + "']") : false;
-                if (points)
-                points.each(function(point){
-                    var option = new Option({
-                        elem : $(this),
-                        parent : self
-                    });
-                    result.push(option);
-                });
-                return result;
+            this.operator = option.attr('data-price-operator');
+
+            this.price = (function(){
+                var price = option.attr('data-price-value');
+                if (price) return parseFloat(price);
+                else return 0;
             })();
         }
-        else console.error("calcPrice must have some elements for init");
     }
-    Price.prototype.recalc = function(option)
+
+    class Price
     {
-        this.addToList(option);
-        var price = this.price;
-        var update = 0;
-        for (var i in this.options)
+        constructor(options)
         {
-            var option = this.options[i];
-            switch (option.operator)
-            {
-                case "+"  : update += option.price; break;
-                case "-"  : update -= option.price; break;
-                case "*"  :
-                    if (option.calctype == "update") price *= option.price;
-                    else if (option.calctype == "static")
-                    {
-                        price = 0;
-                        update += this.price * option.price;
-                    }
-                    break;
-                case "/"  :
-                    if (option.calctype == "update") price /= option.price;
-                    else if (option.calctype == "static")
-                    {
-                        price = 0;
-                        update += this.price / option.price;
-                    }
-                    break;
-                case "+%" :
-                    if (option.calctype == "update") update += (update / 100) * option.price;
-                    else if (option.calctype == "static") update += (this.price / 100) * option.price;
-                    break;
-                case "-%" :
-                    if (option.calctype == "update") update -= (update / 100) * option.price;
-                    else if (option.calctype == "static") update -= (this.price / 100) * option.price;
-                    break;
-            }
-        }
-        price += update;
-        this.updatePrice = price;
-        this.insertPrice(price);
-        if (this.old) this.old.text(Math.round(this.factor * this.updatePrice) + this.currency);
-    }
-    Price.prototype.addToList = function(option)
-    {
-        var self = this;
-        if (option.type == "radio")
-        {
-            if (option.price)
-            {
-                clearNamedOptions(option);
-                if (!(option.id in this.options)) this.options[option.id] = option;
-            }
-            else clearNamedOptions(option);
-        }
-        else if (option.type == "checkbox")
-        {
-            if (option.id in this.options) delete this.options[option.id];
-            else this.options[option.id] = option;
-        }
-        else if (option.type == "select")
-        {
-            if (option.id in this.options)
-            {
-                if (option.price) this.options[option.id] = option;
-                else delete this.options[option.id];
-            }
-            else this.options[option.id] = option;
+            this.$element;
+            this.price;
+            this.currency;
+            this.updatePrice;
+            this.points;
+            this.old;
+            this.oldPrice;
+            this.options = {};
+            this.factor;
+            this._init(options);
         }
 
-        function clearNamedOptions(option)
+        _init(options)
         {
-            for (var i in self.options)
+            var self = this;
+
+            this.$element = options.$element;
+
+            if (this.$element)
             {
-                if (self.options[i].type == option.type)
-                if (self.options[i].name == option.name)
-                delete self.options[i];
+                this.price = parseFloat(this.$element.attr("data-price-value")) || 0;
+
+                this.currency = this.$element.attr('data-price-currency') || "";
+
+                this.old = (function(){
+                        var old = self.$element.attr('data-price-old');
+                            old ? old = $(old) : old = false;
+                        if (old) return old;
+                        else return false;
+                    })();
+
+                this.oldPrice = self.old ? parseFloat(self.old.attr("data-price-value")) || 0 : 0;
+
+                this.factor = (function(){
+                        if (self.old) return self.oldPrice / self.price;
+                        else return 0;
+                    })();
+
+                this.points = (function(){
+                    var result = [];
+                    var points = self.$element.attr('data-price-points') || false;
+                        points ? points = $("[data-price-target='" + points + "']") : false;
+                    if (points)
+                    points.each(function(point){
+                        var option = new Option({
+                            $element : $(this),
+                            parent : self
+                        });
+                        result.push(option);
+                    });
+                    return result;
+                })();
+
+            }
+            else
+            {
+                console.error("calcPrice must have some elements for init");
+            }
+
+        }
+
+        recalc(option)
+        {
+            this._addToList(option);
+
+            var price = this.price,
+                update = 0;
+
+            for (var i in this.options)
+            {
+                var option = this.options[i];
+                switch (option.operator)
+                {
+                    case "+"  : update += option.price; break;
+                    case "-"  : update -= option.price; break;
+                    case "*"  :
+                        if (option.calctype == "update") price *= option.price;
+                        else if (option.calctype == "static")
+                        {
+                            price = 0;
+                            update += this.price * option.price;
+                        }
+                        break;
+                    case "/"  :
+                        if (option.calctype == "update") price /= option.price;
+                        else if (option.calctype == "static")
+                        {
+                            price = 0;
+                            update += this.price / option.price;
+                        }
+                        break;
+                    case "+%" :
+                        if (option.calctype == "update") update += (price / 100) * option.price;
+                        else if (option.calctype == "static") update += (this.price / 100) * option.price;
+
+                        console.log(update);
+
+                        break;
+                    case "-%" :
+                        if (option.calctype == "update") update -= (price / 100) * option.price;
+                        else if (option.calctype == "static") update -= (this.price / 100) * option.price;
+                        break;
+                }
+            }
+
+            price += update;
+            this.updatePrice = price;
+
+            this._insertPrice(price);
+
+            if (this.old)
+            {
+                this.old.text(Math.round(this.factor * this.updatePrice) + this.currency);
             }
         }
-    }
-    Price.prototype.insertPrice = function(price)
-    {
-        this.elem.val(price + this.currency);
-        this.elem.text(price + this.currency);
+
+        _addToList(option)
+        {
+            var self = this;
+            if (option.type == "radio")
+            {
+                if (option.price)
+                {
+                    clearNamedOptions(option);
+                    if (!(option.id in this.options)) this.options[option.id] = option;
+                }
+                else clearNamedOptions(option);
+            }
+            else if (option.type == "checkbox")
+            {
+                if (option.id in this.options) delete this.options[option.id];
+                else this.options[option.id] = option;
+            }
+            else if (option.type == "select")
+            {
+                if (option.id in this.options)
+                {
+                    if (option.price) this.options[option.id] = option;
+                    else delete this.options[option.id];
+                }
+                else this.options[option.id] = option;
+            }
+
+            function clearNamedOptions(option)
+            {
+                for (var i in self.options)
+                {
+                    if (self.options[i].type == option.type)
+                    if (self.options[i].name == option.name)
+                    delete self.options[i];
+                }
+            }
+        }
+
+        _insertPrice(price)
+        {
+            this.$element.val(price + this.currency);
+            this.$element.text(price + this.currency);
+        }
     }
 
     $.fn.priceupdate = function(options)
     {
         this.each(function(){
-            if (options) options.elem = $(this);
-            else options = { elem: $(this) };
+            if (options) options.$element = $(this);
+            else options = { $element: $(this) };
             this.price = new Price(options);
         });
     }
